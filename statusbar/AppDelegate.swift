1987b0d43892e1970f8e82435d5ee56ce5bf5e5e
Spiros Raptis <spirosrap@gmail.com>
2016-11-14 18:27:57 +0200

    Initial Commit

---
+++ statusbar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  statusbar
+//
+//  Created by spiros on 14/11/2016.
+//  Copyright © 2016 spiros. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
